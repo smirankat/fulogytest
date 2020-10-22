ab7ce65f3a0a06548ef0c62c528052218451585c
Your Name <you@example.com>
2020-10-22 20:00:19 +0300

    Updates

---
+++ precache-manifest.a592eb3b7d88e0cfeb4431c3cde09c30.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc0543582fc27b73a87ad673481d9954",
+    "url": "/fulogytest/index.html"
+  },
+  {
+    "revision": "7a548465eab3150c48bc",
+    "url": "/fulogytest/static/css/main.8b843f76.chunk.css"
+  },
+  {
+    "revision": "06cb07c1c15ed404af0b",
+    "url": "/fulogytest/static/js/2.0b4529f9.chunk.js"
+  },
+  {
+    "revision": "e465a1d90cd90ed95cee5d0fa63a8915",
+    "url": "/fulogytest/static/js/2.0b4529f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a548465eab3150c48bc",
+    "url": "/fulogytest/static/js/main.7d352aee.chunk.js"
+  },
+  {
+    "revision": "fcb603074b5d44f84b0a",
+    "url": "/fulogytest/static/js/runtime-main.aa63a5c8.js"
+  },
+  {
+    "revision": "7586fe6a841121dcdc47b25a39114924",
+    "url": "/fulogytest/static/media/Rectangle1.7586fe6a.png"
+  },
+  {
+    "revision": "664e110dd2fa98e47b0765b3aa72cb36",
+    "url": "/fulogytest/static/media/open-sans-v18-latin_cyrillic-600.664e110d.woff"
+  },
+  {
+    "revision": "c8696f38f823e82d8c9142839d0e5d5f",
+    "url": "/fulogytest/static/media/open-sans-v18-latin_cyrillic-600.c8696f38.woff2"
+  },
+  {
+    "revision": "2491afe95f7cac40010a141ae5c5fc77",
+    "url": "/fulogytest/static/media/open-sans-v18-latin_cyrillic-regular.2491afe9.woff"
+  },
+  {
+    "revision": "aeb4b1bfc96bce2be7d95d9917d8cccb",
+    "url": "/fulogytest/static/media/open-sans-v18-latin_cyrillic-regular.aeb4b1bf.woff2"
+  }
+]);
